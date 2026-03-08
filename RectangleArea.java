b501918f82955b5febc4b616a1acd8783fe66a82
SoHpNiL <shopnil.rhm@gmail.com>
2026-03-09 01:21:39 +1300

    Bug Fix RecArea

--- RectangleArea.java
+++ RectangleArea.java
@@ -1,10 +1,28 @@
 class RectangleArea{
 public static void main(String[] args){
-    int result6 = calculateRectangleArea(-3, -7);
-    System.out.println("The rectangular area is " + result6);
-
     int result1 = calculateRectangleArea(5, 6);
     System.out.println("The rectangular area is " + result1);
+    
+    int result2 = calculateRectangleArea(3, 7);
+    System.out.println("The rectangular area is " + result2);
+   
+    int result3 = calculateRectangleArea(0, 7);
+    System.out.println("The rectangular area is " + result3);
+    
+    int result4 = calculateRectangleArea(-3, 7);
+    System.out.println("The rectangular area is " + result4);
+   
+    int result5 = calculateRectangleArea(2, -7);
+    System.out.println("The rectangular area is " + result5);
+   
+    int result6 = calculateRectangleArea(-3, -7);
+    System.out.println("The rectangular area is " + result6);
+   
+    int result7 = calculateRectangleArea(0, 0);
+    System.out.println("The rectangular area is " + result7);
+   
+    int result8 = calculateRectangleArea(5, 0);
+    System.out.println("The rectangular area is " + result8);
 }
 
 
@@ -19,7 +37,7 @@ public static int calculateRectangleArea(int width, int height) {
         System.out.println("Height is less than 0, setting it to 0");
     }
 
-    else{
+    if (width >=0 && height >= 0){
         int area = width * height;
         return area;
     }
