3e81e6ecc203836eb85fcbcc5ce70b372d0cdcc8
SoHpNiL <shopnil.rhm@gmail.com>
2026-03-09 00:45:09 +1300

    Commit #2 (L1 Updated)

--- L1.java
+++ L1.java
@@ -1,32 +1,35 @@
 class L1 {
-  public static void main(String[] args) {
-    
-    int[] numbers = {2,3,5,7,8};     
-    int result = ArraySum(numbers);
-
-    if (result < 5 && result != 0){
-      System.out.println("under");
-    }
-
-    else if (result > 30){
-      System.out.println("over");
-    }
-
-    else if (result == 0){
-      System.out.println("empty");
-    }
-    else{
-      System.out.println(result);
-    }
+ public static void main(String[] args) {
+      sumOfArray(new int[]{});
+      sumOfArray(new int[]{3});
+      sumOfArray(new int[]{23});
+      sumOfArray(new int[]{-5});
+      sumOfArray(new int[]{-3, -2, 1});
+      sumOfArray(new int[]{2, 4, 8, -2, 1});
   }
 
 
 
-  public static int ArraySum(int[] numbers) {
+  public static void sumOfArray(int[] numbers) {
     int sum = 0;
     for (int i = 0; i < numbers.length; i++) {
       sum += numbers[i];
     }
-    return sum;
+
+    if (sum < 5 && sum != 0){
+      System.out.println("under");
+    }
+
+    else if (sum > 30){
+      System.out.println("over");
+    }
+
+    else if (sum == 0){
+      System.out.println("empty");
+    }
+    else{
+      System.out.println(sum);
+    }
   }
 }
+  
